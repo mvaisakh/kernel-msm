129a8f99891b411e96e019d505221b35f5cb872a
ATechnoHazard <amolele@firehound.org>
2018-08-17 19:21:36 +0200

    drivers: fed up of this single error.
    What harm could it cause anyways
    
    Signed-off-by: ATechnoHazard <amolele@firehound.org>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -2604,7 +2604,7 @@ set_done:
 	return ret;
 }
 
-int32_t tsens_get_temp(struct tsens_device *dev, long *temp);
+/*int32_t tsens_get_temp(struct tsens_device *dev, int *temp);*/
 
 static int therm_get_temp(uint32_t id, enum sensor_id_type type, int *temp)
 {
@@ -2618,13 +2618,13 @@ static int therm_get_temp(uint32_t id, enum sensor_id_type type, int *temp)
 	}
 
 	switch (type) {
-	case THERM_ZONE_ID:
+	/*case THERM_ZONE_ID:
 		ret = sensor_get_temp(id, temp);
 		if (ret) {
 			pr_err("Unable to read thermal zone sensor:%d\n", id);
 			goto get_temp_exit;
 		}
-		break;
+		break;*/
 	case THERM_TSENS_ID:
 		tsens_dev.sensor_num = id;
 		ret = tsens_get_temp(&tsens_dev, &temp);
