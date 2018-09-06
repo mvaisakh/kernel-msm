f1e05d3f84fe06ae5dd9096855c13a13f29ca5e3
Vaisakh Murali <vaisakhmurali@gmail.com>
2018-09-06 16:45:33 +0200

    Revert "ASoC: pil-q6v5-mss: Export modem ssr reason."
    This reverts commit 105dce5fb3ea495b836f0244aaaf205e1f74f147.

--- drivers/soc/qcom/pil-q6v5-mss.c
+++ drivers/soc/qcom/pil-q6v5-mss.c
@@ -41,14 +41,10 @@
 
 #define subsys_to_drv(d) container_of(d, struct modem_data, subsys_desc)
 
-static char pil_ssr_reason[MAX_SSR_REASON_LEN];
-static char *ssr_reason = pil_ssr_reason;
-module_param(ssr_reason, charp, S_IRUGO);
-
 static void log_modem_sfr(void)
 {
 	u32 size;
-	char *smem_reason;
+	char *smem_reason, reason[MAX_SSR_REASON_LEN];
 
 	smem_reason = smem_get_entry_no_rlock(SMEM_SSR_REASON_MSS0, &size, 0,
 							SMEM_ANY_HOST_FLAG);
@@ -63,7 +59,6 @@ static void log_modem_sfr(void)
 
 	strlcpy(reason, smem_reason, min(size, MAX_SSR_REASON_LEN));
 	pr_err("modem subsystem failure reason: %s.\n", reason);
-
 }
 
 static void restart_modem(struct modem_data *drv)
